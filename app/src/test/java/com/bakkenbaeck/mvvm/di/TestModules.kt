ffedcfc18ea9d4fba5c5091afac9c1bd761c62a7
csdodd <colinsdodd@gmail.com>
2017-11-09 19:20:02 +0100

    Mock Database in unit tests.
    Fixes previously broken tests.

--- app/src/test/java/com/bakkenbaeck/mvvm/di/TestModules.kt
+++ app/src/test/java/com/bakkenbaeck/mvvm/di/TestModules.kt
@@ -1,15 +1,23 @@
 package com.bakkenbaeck.mvvm.di
 
+import com.bakkenbaeck.mvvm.model.database.CommentDao
 import com.bakkenbaeck.mvvm.model.database.Database
 import com.bakkenbaeck.mvvm.model.repository.CommentsRepository
 import com.bakkenbaeck.testUtil.CommentsServiceGenerator
+import org.mockito.Mockito
 
 
 class TestModules: ModuleProvider {
     override fun baseUrl() = "http://example.com/"
     override fun commentsRepo() = CommentsRepository(CommentsServiceGenerator().commentsService)
-    override fun database(): Database {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun database() = mockDatabase()
     override fun scheduler() = TestSchedulerProvider()
+
+    private fun mockDatabase(): Database {
+        val commentDao = Mockito.mock(CommentDao::class.java)
+        Mockito.`when`(commentDao.getAllComments()).thenReturn(emptyList())
+        val db = Mockito.mock(Database::class.java)
+        Mockito.`when`(db.commentDao()).thenReturn(commentDao)
+        return db
+    }
 }
